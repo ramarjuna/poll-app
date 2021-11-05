a497aa68b7f4062fd14f942a14271e6629fd136d
“ramarjuna” <ramarjuna.07@gmail.com>
2021-11-05 21:25:01 +0530

    updated votes method. App works with votes.

--- polls/views.py
+++ polls/views.py
@@ -33,16 +33,20 @@ def results(request, question_id):
     return render(request, 'polls/results.html', {'question': question})
   
 def vote(request, question_id):
-  # return HttpResponse('Vote Page' % question_id)
-  question = get_object_or_404(request, pk=question_id)
-  try:
-    selected_choice = question.choice_set.get(pk=request.POST['choice'])
-  except (KeyError, Choice.DoesNotExist):
-    return render(request, 'polls/detail.html', {
-      'question': question,
-      'error_message': 'You didnt select a choice',
-    })
-  else:
-    selected_choice += 1
-    selected_choice.save()
-    return HttpResponseRedirect(reverse('polls:results',args=(question.id)))
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        print(selected_choice.votes)
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
